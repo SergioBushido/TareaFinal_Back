4ef20dba7ec08b6284f9e6c9503de12603b94463
Sergio <sergio.ume@hotmail.com>
2024-04-16 14:45:59 +0100

    Correcciones

--- src/main/java/com/gestor/gestortareasbackend/model/User.java
+++ src/main/java/com/gestor/gestortareasbackend/model/User.java
@@ -1,34 +1,73 @@
 package com.gestor.gestortareasbackend.model;
 
+
+import com.gestor.gestortareasbackend.model.role.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collection;
+import java.util.List;
 
-@Entity
-@Table(name = "users")
 @Data
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    private long id;
 
-    @Column(unique = true)
     private String username;
-
+    private String email;
     private String password;
 
-    //Un usuasrio puede tener muchos proyectos
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<Project> projects = new HashSet<>();
-
-    //muchos usuarios pueden tener un solo rol
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "role_id")
     private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Token> tokens;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
